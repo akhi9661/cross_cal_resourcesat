05e15f221fd4b83ba08d6a0e7b8204b22e89c8b0
akhi9661 <akhiraj9661@gmail.com>
2023-04-16 01:06:52 +0530

    Created main.py

---
+++ cross_cal_resourcesat/main.py
@@ -0,0 +1,15 @@
+from cross_cal_resourcesat.landsat import *
+from cross_cal_resourcesat.cross_cal_resourcesat import *
+
+def main():
+
+    reference_sensor = input('Enter the name of the reference sensor ["Sentinel 2", "Landsat 8", "Others"]: ')
+    inpf_liss = input('Enter the path to the folder containing the radiance images of LISS III or AWiFS [inpf_liss]: ')
+    if reference_sensor == 'Others':
+        print('Note: If "reference_sensor" is "Others", "inpf_ref" should be the path to reflectance images.')
+    inpf_ref = input('Enter the path to the folder containing the reference images [inpf_ref]:')
+
+    do_calibration(inpf_liss = inpf_liss, inpf_ref = inpf_ref, reference_sensor = reference_sensor)
+
+if __name__ == '__main__':
+    main()
