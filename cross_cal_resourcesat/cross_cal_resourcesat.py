adb9962c1c86028b311dd5f044403f90b5ce5cd7
akhi9661 <akhiraj9661@gmail.com>
2023-04-16 01:06:37 +0530

    Updated cross_cal_resourcesat

--- cross_cal_resourcesat/cross_cal_resourcesat.py
+++ cross_cal_resourcesat/cross_cal_resourcesat.py
@@ -4,7 +4,7 @@
 import rasterio, os, re, math, glob, shutil
 from osgeo import gdal, osr, gdalconst
 import numpy as np 
-from cross_cal_resourcesat.landsat import *
+from .landsat import landsat_ref
 
 def meta(inpf, keyword):
 
@@ -291,14 +291,3 @@ def do_calibration(inpf_liss, inpf_ref, reference_sensor):
     sbaf, cal_liss, ref_liss, ref_band = calc_calibration(op_liss, op_ref)
     
     return None
-    
-    
-if __name__ == '__main__':
-
-    reference_sensor = input('Enter the name of the reference sensor ["Sentinel 2", "Landsat 8", "Others"]: ')
-    inpf_liss = input('Enter the path to the folder containing the radiance images of LISS III or AWiFS [inpf_liss]: ')
-    if reference_sensor == 'Others':
-        print('Note: If "reference_sensor" is "Others", "inpf_ref" should be the path to reflectance images.')
-    inpf_ref = input('Enter the path to the folder containing the reference images [inpf_ref]:')
-
-    do_calibration(inpf_liss = inpf_liss, inpf_ref = inpf_ref, reference_sensor = reference_sensor)
